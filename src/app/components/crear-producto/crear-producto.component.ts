4cd33b4bb9a8018f34b64fee1883cb3f85960900
Fernando Torres <fernandot371@gmail.com>
2021-05-24 22:11:41 -0500

    sec3 28

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-crear-producto',
@@ -15,7 +16,8 @@ export class CrearProductoComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private _productoService: ProductoService
     ) { 
     this.productoForm = this.fb.group({
       producto:['', Validators.required],
@@ -37,10 +39,13 @@ export class CrearProductoComponent implements OnInit {
       ubicacion: this.productoForm.get('ubicacion')?.value,
       precio: this.productoForm.get('precio')?.value,
     }
-    console.log(PRODUCTO);
-    this.toastr.success('El producto fue registrado con éxito!', 'Producto registrado(success)!'); // mensaje & titulo
-    //this.toastr.info('El producto fue registrado con éxito!', 'Producto registrado(info)!'); // mensaje & titulo
-    this.router.navigate(["/"]);
+    this._productoService.guardarProducto(PRODUCTO).subscribe(data => {
+      this.toastr.success('El producto fue registrado con éxito!', 'Producto registrado(success)!'); // mensaje & titulo
+      this.router.navigate(["/"]);
+    }, error => {
+      console.log(error);
+      this.productoForm.reset();
+    });
   }
 
 }
