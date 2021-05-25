7e4312db375cf1914c1901f37c357eeba1fa4fb3
Fernando Torres <fernandot371@gmail.com>
2021-05-24 22:07:18 -0500

    sec3 27

--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { Producto } from 'src/app/models/producto';
 import { ProductoService } from 'src/app/services/producto.service';
 
@@ -10,7 +11,10 @@ import { ProductoService } from 'src/app/services/producto.service';
 export class ListarProductosComponent implements OnInit {
   listProducto: Producto[] = [];
 
-  constructor(private _productoService:ProductoService) { }
+  constructor(
+    private _productoService:ProductoService,
+    private toastr: ToastrService
+    ) { }
 
   ngOnInit(): void {
     this.obtenerProductos();
@@ -20,6 +24,15 @@ export class ListarProductosComponent implements OnInit {
     this._productoService.getProductos().subscribe(data => {
       console.log(data);
       this.listProducto = data;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  eliminarProducto(id:any) {
+    this._productoService.eliminarProducto(id).subscribe(data => {
+      this.toastr.error("El producto fue eliminado con éxito", "Producto eliminado");
+      this.obtenerProductos();
     }, error => {
       console.log(error);
     })
